316fee94c9533313b8d2c3f75bfda04159fd545b
François-Paul Servant <fps@semanlink.net>
2015-01-26 00:40:26 +0100

    changes in cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,41 +7,113 @@
 ## 	- pass a matrix to createCacheMatrix
 ## 	- call cacheSolve on the returned object to get the inverse of the matrix
 
+## Assumes the matrix is invertible
+##
+## How to use: to cache the inverse of a matrix m
+##  - pass m to createCacheMatrix
+## 	- call cacheSolve on the returned object to get the inverse of m
+## (the inverse is computed only the first time cacheSolve is called:
+## then, it is returned from the cache)
+## Note that if you change m, cacheSolve will still return the inverse of the "initial m"
+## (call set to reset the cache)
 
-## Creates a "matrix" which caches its inverse when it is computed
-## @param x: a matrix
 
+## Create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL # the cache
-	# set the matrix
-        set <- function(y) {
-                x <<- y
-		# reset the cache
-                inv <<- NULL
-        }
-        get <- function() x
-        setInverse <- function(inverse) inv <<- inverse
-        getInverse <- function() inv
-	# return a list with the functions we just created
-        list(set = set, get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
+  makeCacheObject(x, solve)
 }
 
-
-## Returns the inverse of a "matrix" created with makeCacheMatrix.
-## If the inverse has already been calculated (and the matrix has not changed),
+## Returns the inverse of a "special matrix" created with makeCacheMatrix.
+## If the inverse has already been calculated,
 ## the inverse is retrieved from the cache.
-## @param x: created by makeCacheMatrix
-
+## Note that the "..." parameters are used only in the first call
+## (if you change them on next calls, the result won't be changed)
+## @param x: a "special matrix" created by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        inv <- x$getInverse()
-        if(!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setInverse(inv)
-        inv
+  x$getResult(...)
 }
+
+## Create a special object than can cache the value returned by a function fun that takes x as parameter
+## Call $getResult() to get the result (either computing it, or getting it from the cache)
+## Call $set to change the object we're caching bout, and $setFun to change the function whose result is cached
+makeCacheObject <- function(x, fun) {
+  # init the cache
+  # This is important to have the "<<-" work the way we want it to:
+  # <<- looks for a var called cache in the parent environments,
+  # and we want it to find it here
+  cache <- NULL
+  
+  # to be able to check in tests that whether during last call to getResult,
+  # the result has been computed, or got from the cache
+  lastResultGotFromCache <- FALSE
+  
+  # to be used to change the object inside this "special object"
+  set <- function(y) {
+    x <<- y
+    # reset the cache
+    cache <<- NULL
+  }
+  
+  # to be used to change the function whose result is cached
+  setFun <- function(yfun) {
+    fun <<- yfun
+    # reset the cache
+    cache <<- NULL
+  }
+  
+  # return the object we cache about
+  get <- function() x
+  
+  # return the result 
+  getResult <- function(...) {
+    if(!is.null(cache)) {
+      message("getting cached data")
+      lastResultGotFromCache <<- TRUE      
+      return(cache)
+    }
+    lastResultGotFromCache <<- FALSE      
+    cache <<- fun(x, ...)
+  }
+  
+  lastResultHRetrievedFromCache <- function() lastResultGotFromCache
+  
+  # return a list with the functions we just created
+  list(get = get,
+       set = set,
+       setFun = setFun, 
+       lastResultHRetrievedFromCache = lastResultHRetrievedFromCache,
+       getResult = getResult)
+}
+
+## a testthat shows 
+## how it works and that we get some expected results
+testCache <- function() {
+  # identity matrix
+  e <- rbind(c(1, 0), c(0, 1))
+  # a 2*2 matrix
+  mat1 <- rbind(c(1, 3), c(3, 1))
+  # crate a special matrix
+  cm1 <- makeCacheMatrix(mat1)
+  # compute the inverse of mat1
+  inv1 <- cacheSolve(cm1)
+  # inv1 %*% mat1 should be the identity
+  ok <- all(e == inv1 %*% mat1)
+  if (!ok) error("inv1 %*% mat1 should be the identity")
+  else print("inv1 %*% mat1 is the identity matrix")
+  
+  fromCache <- cm1$lastResultHRetrievedFromCache()
+  if (fromCache) error("Unexpected!")
+  else print("Result has been computed")
+  
+  # second time we get the inverse, we should get the same result
+  # and have the message "getting cached data"
+  inv1 <- cacheSolve(cm1)
+  ok <- all(e == inv1 %*% mat1)
+  if (!ok) error("inv1 %*% mat1 should be the identity")
+  else print("inv1 %*% mat1 is the identity matrix")
+  
+  fromCache <- cm1$lastResultHRetrievedFromCache()
+  if (!fromCache) error("Unexpected!")
+  else print("Result has been retrieved from cache")
+}
+
